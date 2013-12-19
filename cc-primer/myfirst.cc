65959cffd1a26e0d7ad8503688b3c5f13f199472
Jim Rice <hitjim@gmail.com>
2013-12-18 22:26:07 -0600

    prepping to upload to github

---
+++ cc-primer/myfirst.cc
@@ -0,0 +1,11 @@
+// myfirst.cc -- displays a message
+
+#include <iostream>                                                     //a PREPROCESSOR directive
+int main()                                                              //function header
+{
+    using namespace std;                                                //make definitions visible
+    cout << "Come up and C++ me some time.  Really book... really???";  //message
+    cout << endl;                                                       //start newline
+    cout << "You won't regret it!" << endl;
+    return 0;                                                           //terminate main
+}
